97d9274dd080329a32200433834d74e9689d0490
Jim Clark <slimslenderslacks@gmail.com>
2020-05-06 22:17:41 -0700

    update lib

--- dev/user.cljs
+++ dev/user.cljs
@@ -1,72 +1,29 @@
 (ns user
   (:require [atomist.main]
-            [atomist.cljs-log :as log]
             [atomist.clojure-tools]
-            [cljs.core.async :refer [<!]])
+            [cljs.core.async :refer [<!]]
+            [atomist.local-runner :refer [call-event-handler fake-push fake-command-handler]])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (enable-console-print!)
 
-(def token (.. js/process -env -API_KEY_SLIMSLENDERSLACKS_PROD_GITHUB_AUTH))
-(def github-token (.. js/process -env -GITHUB_TOKEN))
-
 (comment
-
+  (-> (fake-push "T29E48P34" "atomist-skills" "update-clojure-tools-dependencies-skill" "master")
+      (assoc :configurations [{:parameters [{:name "policy" :value "manualConfiguration"}
+                                            {:name "dependencies" :value "blah"}]}])
+      (call-event-handler atomist.main/handler))
  ;; this should fail because of a bad manualConfiguration
-  (atomist.main/handler #js {:data {:Push [{:branch "master"
-                                            :repo {:name "update-clojure-tools-dependencies-skill"
-                                                   :org {:owner "atomist-skills"
-                                                         :scmProvider {:providerId "zjlmxjzwhurspem"
-                                                                       :credential {:secret github-token}}}}
-                                            :after {:message ""}}]}
-                             :secrets [{:uri "atomist://api-key" :value token}]
-                             :configurations [{:parameters [{:name "policy" :value "manualConfiguration"}
-                                                            {:name "dependencies" :value "blah"}]}]
-                             :extensions {:team_id "T29E48P34"
-                                          :correlation_id "corrid"}}
-                        (fn [& args] (log/info "sendreponse " args)))
 
- ;; this should work
-  (atomist.main/handler #js {:data {:Push [{:branch "master"
-                                            :repo {:name "update-clojure-tools-dependencies-skill"
-                                                   :org {:owner "atomist-skills"
-                                                         :scmProvider {:providerId "zjlmxjzwhurspem"
-                                                                       :credential {:secret github-token}}}}
-                                            :after {:message ""}}]}
-                             :secrets [{:uri "atomist://api-key" :value token}]
-                             :configurations [{:parameters [{:name "policy" :value "manualConfiguration"}
-                                                            {:name "dependencies" :value "[[org.clojure/clojure \"1.10.2\"]]"}]}]
-                             :extensions {:correlation_id "corrid"
-                                          :team_id "T29E48P34"}}
-                        (fn [& args] (log/info "sendreponse " args)))
 
- ;; command handler to extract
-  (atomist.main/handler #js {:command "ShowToolsDepsDependencies"
-                             :source {:slack {:channel {:id "C012TCG93HN"}
-                                              :user {:id "U2ATJPCSK"}
-                                              :team {:id "T29E48P34"}}}
-                             :team {:id "T29E48P34" :name "atomist-community"}
-                             :correlation_id "corrid"
-                             :api_version "1"
-                             :raw_message "clj fingerprints"
-                             :secrets [{:uri "atomist://api-key" :value token}]}
-                        (fn [& args] (log/info "sendreponse " args)))
+  (-> (fake-push "T29E48P34" "atomist-skills" "update-clojure-tools-dependencies-skill" "master")
+      (assoc :configurations [{:parameters [{:name "policy" :value "manualConfiguration"}
+                                            {:name "dependencies" :value "[[org.clojure/clojure \"1.10.2\"]]"}]}])
+      (call-event-handler atomist.main/handler))
 
- ;; command handler to update
-  (atomist.main/handler #js {:command "UpdateToolsDepsDependency"
-                             :source {:slack {:channel {:id "C012TCG93HN"}
-                                              :user {:id "U2ATJPCSK"}
-                                              :team {:id "T29E48P34"}}}
-                             :team {:id "T29E48P34" :name "atomist-community"}
-                             :correlation_id "corrid"
-                             :api_version "1"
-                             :parameters [{:name "dependency" :value "[org.clojure/clojurescript \"1.10.522\"]"}]
-                             :raw_message "clj update"
-                             :secrets [{:uri "atomist://api-key" :value token}]}
-                        (fn [& args] (cljs.pprint/pprint (js->clj (first args)))))
+  (-> (fake-command-handler "T29E48P34" "ShowToolsDepsDependencies" "clj fingerprints" "C012TCG93HN" "U2ATJPCSK")
+      (call-event-handler atomist.main/handler))
 
-  (go (println (<! (atomist.graphql-channels/head-commits->channel
-                    {:secrets [{:uri "atomist://api-key" :value token}]
-                     :team {:id "AK748NQC5"}}
-                    "clojure-tools-deps"
-                    "com.atomist::api-cljs")))))
+  (-> (fake-command-handler "T29E48P34" "ShowToolsDepsDependencies" "clj fingerprints" "C012TCG93HN" "U2ATJPCSK")
+      (assoc :parameters [{:name "dependency" :value "[org.clojure/clojurescript \"1.10.522\"]"}])
+      (call-event-handler atomist.main/handler))
+  )
