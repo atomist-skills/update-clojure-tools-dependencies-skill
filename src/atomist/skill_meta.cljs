abd1cd08dc4e7ef61f20acc119cfa1f17e529725
Jim Clark <slimslenderslacks@gmail.com>
2020-05-20 20:17:43 -0700

    Bump update-clojure-tools-dependencies-skill to 0.1.16 and spit metadata

--- src/atomist/skill_meta.cljs
+++ src/atomist/skill_meta.cljs
@@ -3,6 +3,6 @@
 
 (def module-name "update-clojure-tools-dependencies-skill")
 (def path ".")
-(def version "0.1.15")
-(def tag "0.1.15")
-(def generated-at "2020-05-21T03:16:53Z")
+(def version "0.1.16")
+(def tag "0.1.16")
+(def generated-at "2020-05-21T03:17:43Z")
